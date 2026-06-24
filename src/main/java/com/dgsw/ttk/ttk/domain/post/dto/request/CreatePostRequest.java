d3f9c8459ed6e449a30ba6187849cef99468266b
장강민 <jkm9780533@gmail.com>
2026-06-24 14:54:00 +0900

    create PostRequest

--- src/main/java/com/dgsw/ttk/ttk/domain/post/dto/request/CreatePostRequest.java
+++ src/main/java/com/dgsw/ttk/ttk/domain/post/dto/request/CreatePostRequest.java
@@ -1,4 +1,25 @@
 package com.dgsw.ttk.ttk.domain.post.dto.request;
 
-public class CreatePostRequest {
+import com.dgsw.ttk.ttk.domain.post.entity.Post;
+import com.dgsw.ttk.ttk.domain.post.entity.Status;
+
+import java.util.UUID;
+
+public record CreatePostRequest(
+
+        String studentId,
+        String studentName,
+        String issue
+) {
+
+    public Post toEntity(CreatePostRequest createPostRequest) {
+
+        return Post.builder()
+                .id(UUID.randomUUID())
+                .studentId(createPostRequest.studentId)
+                .studentName(createPostRequest.studentName)
+                .issue(createPostRequest.issue)
+                .status(Status.WAIT)
+                .build();
+    }
 }
