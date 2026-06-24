3cc38a62eaf716ed7bdd2fe522d329d8e3490eb7
장강민 <jkm9780533@gmail.com>
2026-06-24 14:55:09 +0900

    CreatePost

--- src/main/java/com/dgsw/ttk/ttk/domain/post/service/usecase/CreatePost.java
+++ src/main/java/com/dgsw/ttk/ttk/domain/post/service/usecase/CreatePost.java
@@ -1,4 +1,9 @@
 package com.dgsw.ttk.ttk.domain.post.service.usecase;
 
+import com.dgsw.ttk.ttk.domain.post.dto.request.CreatePostRequest;
+import com.dgsw.ttk.ttk.domain.post.dto.response.CreatePostResponse;
+
 public interface CreatePost {
+
+    CreatePostResponse createPost(CreatePostRequest createPostRequest);
 }
