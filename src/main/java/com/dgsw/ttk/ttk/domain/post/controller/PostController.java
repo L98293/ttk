066d1466071c0ad7d2b6a1a3c33cce269cba36e8
장강민 <jkm9780533@gmail.com>
2026-06-24 14:53:09 +0900

    post controller

--- src/main/java/com/dgsw/ttk/ttk/domain/post/controller/PostController.java
+++ src/main/java/com/dgsw/ttk/ttk/domain/post/controller/PostController.java
@@ -1,4 +1,24 @@
 package com.dgsw.ttk.ttk.domain.post.controller;
 
+import com.dgsw.ttk.ttk.domain.post.dto.request.CreatePostRequest;
+import com.dgsw.ttk.ttk.domain.post.dto.response.CreatePostResponse;
+import com.dgsw.ttk.ttk.domain.post.service.usecase.CreatePost;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class PostController {
+
+    @Autowired
+    private CreatePost createPost;
+
+    @PostMapping("/post")
+    public ResponseEntity<CreatePostResponse> createPost(CreatePostRequest createPostRequest) {
+
+        return ResponseEntity.ok(createPost.createPost(createPostRequest));
+    }
 }
