517055c396cd4a3236dbd242b205960e8c4d0f0e
장강민 <jkm9780533@gmail.com>
2026-06-24 14:55:22 +0900

    PostService

--- src/main/java/com/dgsw/ttk/ttk/domain/post/service/PostService.java
+++ src/main/java/com/dgsw/ttk/ttk/domain/post/service/PostService.java
@@ -1,4 +1,23 @@
 package com.dgsw.ttk.ttk.domain.post.service;
 
-public class PostService {
+import com.dgsw.ttk.ttk.domain.post.dto.request.CreatePostRequest;
+import com.dgsw.ttk.ttk.domain.post.dto.response.CreatePostResponse;
+import com.dgsw.ttk.ttk.domain.post.repository.PostRepository;
+import com.dgsw.ttk.ttk.domain.post.service.usecase.CreatePost;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService implements CreatePost {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Override
+    public CreatePostResponse createPost(CreatePostRequest createPostRequest) {
+
+        postRepository.save(createPostRequest.toEntity(createPostRequest));
+
+        return CreatePostResponse.of("제출되었습니다.");
+    }
 }
