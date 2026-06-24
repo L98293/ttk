e7f6c24d1489a152efa159399b7c9e861e110a43
김경윤 <l982930009@gmail.com>
2026-06-24 14:06:11 +0900

    first commit

---
+++ src/test/java/com/dgsw/ttk/ttk/TtkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgsw.ttk.ttk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
