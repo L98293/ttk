8d6ea5e63d0c24568cda27feff0183d861138c3f
장강민 <jkm9780533@gmail.com>
2026-06-24 14:54:22 +0900

    CreatePostResponse

--- src/main/java/com/dgsw/ttk/ttk/domain/post/dto/response/CreatePostResponse.java
+++ src/main/java/com/dgsw/ttk/ttk/domain/post/dto/response/CreatePostResponse.java
@@ -1,4 +1,12 @@
 package com.dgsw.ttk.ttk.domain.post.dto.response;
 
-public class CreatePostResponse {
+public record CreatePostResponse(
+
+        String message
+) {
+
+    public static CreatePostResponse of(String message) {
+
+        return new CreatePostResponse(message);
+    }
 }
